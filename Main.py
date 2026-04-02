73bf8ccffa4efdc0b9e30a5c4a784dc01e4dfa24
Rachel Gerencser <rachel.gerencser@gmail.com>
2026-04-01 23:24:50 -0400

    Update Main.py to test dynamic venue coordinates for weather data retrieval

--- Main.py
+++ Main.py
@@ -29,7 +29,12 @@ from DataAccess.VenueRepository import VenueRepository
 venue_name = "McDonald's"
 venue_address = "Ocean Ave, San Francisco"
 
+venue_repository = VenueRepository()
+venue_data_object = venue_repository.get_cached_data(venue_name, venue_address)
+venue_data_object.print_venue_data()
+
+
 weather_api_client = WeatherAPIClient()
-weather_data_object = weather_api_client.get_weather_data(37.7749, -122.4194)
+weather_data_object = weather_api_client.get_weather_data(venue_data_object.latitude, venue_data_object.longitude)
 weather_data_object.print_weather_data()
 
